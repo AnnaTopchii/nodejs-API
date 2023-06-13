c81efdb43a5c8f349bd1f5d136cd55d730222f91
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-06-14 00:27:29 +0300

    ADD: logout, getCurrent

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,8 +12,8 @@ router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
-// router.get("/current", authenticate, ctrl.getCurrent);
+router.get("/current", authenticate, ctrl.getCurrent);
 
-// router.post("/logout", authenticate, ctrl.logout);
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
